ee73ddf189d3f118ee1dec6f2721274f9605a145
Bernardo Lansing <bernardolans@gmail.com>
2023-12-15 11:05:46 -0300

    basic theming and favicon

--- lib/main.dart
+++ lib/main.dart
@@ -15,13 +15,29 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'ReLer',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+      theme: _theme,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
+
+final _theme = ThemeData(
+  brightness: Brightness.light,
+  colorScheme: const ColorScheme(
+    brightness: Brightness.light,
+    primary: Color(0xFF718E76),
+    onPrimary: Colors.white,
+    secondary: Color(0xFF4F493D),
+    onSecondary: Colors.white,
+    background: Color(0xFFE1DBC9),
+    onBackground: Colors.black,
+    error: Color(0xFFBB2020),
+    onError: Colors.white,
+    surface: Color(0xFFD6CAA7),
+    onSurface: Colors.black54
+  ),
+  scaffoldBackgroundColor: const Color(0xFFE1DBC9),
+);
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -45,7 +61,6 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
       body: Center(
