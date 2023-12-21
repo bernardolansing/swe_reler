079d6e84fb98ac6a855190ef5df8daad889f3188
Bernardo Lansing <bernardolans@gmail.com>
2023-12-21 08:29:08 -0300

    navbar component received a dedicated file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:swe_reler/src/screens/landing_page.dart';
+import 'package:swe_reler/src/screens/landing_page/landing_page.dart';
 import 'firebase_options.dart';
 
 void main() async {
---
+++ lib/src/screens/landing_page/landing_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:swe_reler/src/screens/landing_page/navbar.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
+
+  MaterialPageRoute route(BuildContext context) => MaterialPageRoute(
+      builder: (context) => const LandingPage()
+  );
+
+  @override
+  Widget build(BuildContext context) => const Scaffold(
+    body: SingleChildScrollView(child: Column(
+      children: [
+        NavBar(),
+      ],
+    )),
+  );
+}
+
+
