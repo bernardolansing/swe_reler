d06ab3f87550545eafcdfba340fb3e54ad8d8c41
Bernardo Lansing <bernardolans@gmail.com>
2023-12-21 10:09:48 -0300

    temporary fix in navbar to prevent overflow

---
+++ lib/src/screens/landing_page/introduction.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Introduction extends StatelessWidget {
+  const Introduction({super.key});
+
+  @override
+  Widget build(BuildContext context) => Container(
+    decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage('assets/landing-page/introduction-background.jpeg'),
+            fit: BoxFit.cover
+        )
+    ),
+    child: SizedBox(
+      width: double.infinity,
+      height: 550,
+      child: ,
+    ),
+  );
+}
