a169d64e5e8418375331b1dbc4c7243bf86be227
Bernardo Lansing <bernardolans@gmail.com>
2023-12-24 16:08:39 -0300

    added Lispectors flag

--- lib/src/screens/user_dash/side_pannel.dart
+++ lib/src/screens/user_dash/side_pannel.dart
@@ -8,26 +8,89 @@ class SidePannel extends StatelessWidget {
   const SidePannel({super.key, this.loading = false, this.error = false});
 
   @override
-  Widget build(BuildContext context) => ColoredBox(
+  Widget build(BuildContext context) => Container(
       color: Theme.of(context).colorScheme.secondary,
-      child: Flexible(
-        child: ConstrainedBox(
-          constraints: BoxConstraints(
+      child: ConstrainedBox(
+        constraints: BoxConstraints(
+            minWidth: 300,
             maxWidth: 300,
             minHeight: MediaQuery.of(context).size.height
-          ),
-          child: const Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.all(48),
-                child: Align(
-                  alignment: Alignment.centerRight,
-                  child: DrawerMenuButton(),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(48),
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: DrawerMenuButton(),
+              ),
+            ),
+
+            Stack(
+              children: [
+                CustomPaint(
+                    painter: _LispectorsFlagPainter(),
+                    size: const Size(280, 100) // TODO: adjust to fit parent
+                  // width.
                 ),
-              )
-            ],
-          ),
+
+                const Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      _flagUpperText,
+                      SizedBox(height: 16),
+                      Text("100 Lispector's", style: _flagMainTextStyle),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       )
   );
+
+  static const _flagUpperText = Text(
+      'você tem',
+      style: TextStyle(
+        fontSize: 18,
+        fontFamily: 'Poppins',
+        color: Colors.white,
+        fontWeight: FontWeight.w400,
+      )
+  );
+
+  static const _flagMainTextStyle = TextStyle(
+      fontWeight: FontWeight.w400,
+      fontFamily: 'Teko',
+      fontSize: 48,
+      color: Colors.white,
+      height: 0.5,
+  );
+}
+
+class _LispectorsFlagPainter extends CustomPainter {
+  static const _cutoutWidth = 64;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()
+      ..color = const Color(0xFF9B693B)
+      ..style = PaintingStyle.fill;
+
+    Path path = Path()
+      ..lineTo(size.width, 0)
+      ..lineTo(size.width - _cutoutWidth, size.height / 2)
+      ..lineTo(size.width, size.height)
+      ..lineTo(0, size.height)
+      ..lineTo(0, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
