565bf71fae18e388e0cc8e51d966d266dd9344ea
Bernardo Lansing <bernardolans@gmail.com>
2023-12-26 11:46:34 -0300

    added points flag

--- lib/src/screens/user_dash/side_pannel.dart
+++ lib/src/screens/user_dash/side_pannel.dart
@@ -27,6 +27,7 @@ class SidePannel extends StatelessWidget {
               ),
             ),
 
+            // Lispector's flag:
             Stack(
               children: [
                 CustomPaint(
@@ -48,6 +49,35 @@ class SidePannel extends StatelessWidget {
                 ),
               ],
             ),
+
+            const SizedBox(height: 32),
+
+            // Points flag:
+            Stack(
+              children: [
+                CustomPaint(
+                    painter: _PointsArrowPainter(),
+                    size: const Size(280, 100) // TODO: adjust to fit parent
+                  // width.
+                ),
+
+                const Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      _flagUpperText,
+                      SizedBox(height: 16),
+                      Text("100 pontos", style: _flagMainTextStyle),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+
+            const SizedBox(height: 32),
+
+
           ],
         ),
       )
@@ -64,11 +94,11 @@ class SidePannel extends StatelessWidget {
   );
 
   static const _flagMainTextStyle = TextStyle(
-      fontWeight: FontWeight.w400,
-      fontFamily: 'Teko',
-      fontSize: 48,
-      color: Colors.white,
-      height: 0.5,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Teko',
+    fontSize: 48,
+    color: Colors.white,
+    height: 0.5,
   );
 }
 
@@ -85,6 +115,30 @@ class _LispectorsFlagPainter extends CustomPainter {
       ..lineTo(size.width, 0)
       ..lineTo(size.width - _cutoutWidth, size.height / 2)
       ..lineTo(size.width, size.height)
+      ..lineTo(0, size.height)
+      ..lineTo(0, 0);
+
+    canvas.drawPath(path, paint);
+  }
+
+  // TODO: check if we can avoid repainting.
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
+
+class _PointsArrowPainter extends CustomPainter {
+  static const _arrowTipWidth = 64;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()
+      ..color = const Color(0xFF718E76)
+      ..style = PaintingStyle.fill;
+
+    Path path = Path()
+      ..lineTo(size.width - _arrowTipWidth, 0)
+      ..lineTo(size.width, size.height / 2)
+      ..lineTo(size.width - _arrowTipWidth, size.height)
       ..lineTo(0, size.height)
       ..lineTo(0, 0);
 
