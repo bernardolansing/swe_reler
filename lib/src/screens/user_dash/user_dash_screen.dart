76209b00751052a635e07e6a5719712d30bf1146
Bernardo Lansing <bernardolans@gmail.com>
2024-01-01 13:37:25 -0300

    added SubscriptionTag

--- lib/src/screens/user_dash/user_dash_screen.dart
+++ lib/src/screens/user_dash/user_dash_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:swe_reler/src/screens/user_dash/hello_user.dart';
 import 'package:swe_reler/src/screens/user_dash/side_pannel/side_pannel.dart';
+import 'package:swe_reler/src/screens/user_dash/subscription_tag.dart';
 
 class UserDashScreen extends StatefulWidget {
   const UserDashScreen({super.key});
@@ -17,21 +18,23 @@ class _UserDashScreenState extends State<UserDashScreen> {
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Padding(
-            padding: EdgeInsets.fromLTRB(64, 0, 38, 64),
-            child: Column(
-              children: [
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    HelloUser()
-                  ],
-                )
-              ],
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.fromLTRB(64, 0, 38, 64),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      HelloUser(),
+                      SubscriptionTag(),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
-          
-          Spacer(),
 
           SidePannel()
         ],
