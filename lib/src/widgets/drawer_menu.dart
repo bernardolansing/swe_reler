338377dca8b45a631f5ffe2ddac8f9854d8143b6
Bernardo Lansing <bernardolans@gmail.com>
2024-01-22 20:32:32 -0300

    added color and text buttons to drawer

--- lib/src/widgets/drawer_menu.dart
+++ lib/src/widgets/drawer_menu.dart
@@ -4,8 +4,54 @@ class DrawerMenu extends StatelessWidget {
   const DrawerMenu({super.key});
 
   @override
-  Widget build(BuildContext context) => const Drawer(
+  Widget build(BuildContext context) => Drawer(
+    width: 420,
+    shape: const BeveledRectangleBorder(),
+    child: ColoredBox(
+        color: Theme.of(context).colorScheme.primary,
+      child: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 65, vertical: 48),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextButton(
+              onPressed: () {},
+              child: const Text('home'),
+            ),
+            const SizedBox(height: 24),
 
+            TextButton(
+              onPressed: () {},
+              child: const Text('painel de usuário'),
+            ),
+            const SizedBox(height: 24),
+
+            TextButton(
+              onPressed: () {},
+              child: const Text('loja'),
+            ),
+            const SizedBox(height: 24),
+
+            TextButton(
+              onPressed: () {},
+              child: const Text('doe um livro'),
+            ),
+            const SizedBox(height: 24),
+
+            TextButton(
+              onPressed: () {},
+              child: const Text('configurações'),
+            ),
+            const SizedBox(height: 24),
+
+            TextButton(
+              onPressed: () {},
+              child: const Text('logout'),
+            ),
+          ],
+        ),
+      )
+    ),
   );
 }
 
