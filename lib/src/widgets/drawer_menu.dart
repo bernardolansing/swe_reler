ba9e785719896045a001773107a87bacff4da3d2
Bernardo Lansing <bernardolans@gmail.com>
2024-01-23 22:32:50 -0300

    drawer has links to landing page, user dash and is able to invoke logout

--- lib/src/widgets/drawer_menu.dart
+++ lib/src/widgets/drawer_menu.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:swe_reler/src/user.dart';
 
 class DrawerMenu extends StatelessWidget {
   const DrawerMenu({super.key});
+
+  /// Pushes the given route to the Navigator if it isn't already the
+  /// current route.
+  void _pushIfNotCurrent(BuildContext context, String route) {
+    final currentRoute = ModalRoute.of(context)?.settings.name;
+
+    Scaffold.of(context).closeEndDrawer();
+
+    // User requested to navigate to a route other than the current.
+    if (currentRoute != null && ! currentRoute.endsWith(route)) {
+      Navigator.of(context).pushNamed(route);
+    }
+  }
 
   @override
   Widget build(BuildContext context) => Drawer(
@@ -9,62 +23,62 @@ class DrawerMenu extends StatelessWidget {
     shape: const BeveledRectangleBorder(),
     child: ColoredBox(
         color: Theme.of(context).colorScheme.primary,
-      child: SingleChildScrollView(
-        padding: const EdgeInsets.symmetric(horizontal: 65, vertical: 48),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Align(
-              alignment: Alignment.centerRight,
-              child: IconButton(
-                onPressed: Navigator.of(context).pop,
-                style: ButtonStyle(
-                  backgroundColor: MaterialStatePropertyAll(
-                      Colors.white.withAlpha(76)
-                  )
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 65, vertical: 48),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Align(
+                alignment: Alignment.centerRight,
+                child: IconButton(
+                  onPressed: Navigator.of(context).pop,
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll(
+                          Colors.white.withAlpha(76)
+                      )
+                  ),
+                  icon: const Icon(Icons.close, size: 36),
                 ),
-                icon: const Icon(Icons.close, size: 36),
               ),
-            ),
-            const SizedBox(height: 64),
+              const SizedBox(height: 64),
 
-            TextButton(
-              onPressed: () {},
-              child: const Text('home'),
-            ),
-            const SizedBox(height: 24),
+              TextButton(
+                onPressed: () => _pushIfNotCurrent(context, '/'),
+                child: const Text('home'),
+              ),
+              const SizedBox(height: 24),
 
-            TextButton(
-              onPressed: () {},
-              child: const Text('painel de usuário'),
-            ),
-            const SizedBox(height: 24),
+              TextButton(
+                onPressed: () => _pushIfNotCurrent(context, '/dash'),
+                child: const Text('painel de usuário'),
+              ),
+              const SizedBox(height: 24),
 
-            TextButton(
-              onPressed: () {},
-              child: const Text('loja'),
-            ),
-            const SizedBox(height: 24),
+              TextButton(
+                onPressed: () {},
+                child: const Text('loja'),
+              ),
+              const SizedBox(height: 24),
 
-            TextButton(
-              onPressed: () {},
-              child: const Text('doe um livro'),
-            ),
-            const SizedBox(height: 24),
+              TextButton(
+                onPressed: () {},
+                child: const Text('doe um livro'),
+              ),
+              const SizedBox(height: 24),
 
-            TextButton(
-              onPressed: () {},
-              child: const Text('configurações'),
-            ),
-            const SizedBox(height: 24),
+              TextButton(
+                onPressed: () {},
+                child: const Text('configurações'),
+              ),
+              const SizedBox(height: 24),
 
-            TextButton(
-              onPressed: () {},
-              child: const Text('logout'),
-            ),
-          ],
-        ),
-      )
+              TextButton(
+                onPressed: () => AppUser.logout(context),
+                child: const Text('logout'),
+              ),
+            ],
+          ),
+        )
     ),
   );
 }
