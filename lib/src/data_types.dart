eadd5fa6bc23a851d718386ff8f97a9ba3055679
Bernardo Lansing <bernardolans@gmail.com>
2024-02-03 18:20:19 -0300

    created Purchase data type

--- lib/src/data_types.dart
+++ lib/src/data_types.dart
@@ -20,3 +20,38 @@ enum DonationStatus {
   underInspection,
   finished,
 }
+
+class Purchase {
+  final List<PurchasedBook> books;
+  final DateTime date;
+  final double totalPrice;
+
+  Purchase(this.books) :
+      date = DateTime.now(),
+      totalPrice = books
+          .fold(0, (acc, elem) => acc + elem.unitPrice * elem.amount);
+
+  Purchase.fromEntry(Map entry) :
+      books = (entry['books'] as List)
+          .map((entry) => PurchasedBook.fromEntry(entry))
+          .toList(growable: false),
+      date = (entry['lastUpdate'] as Timestamp).toDate(),
+      totalPrice = entry['totalPrice'];
+}
+
+class PurchasedBook {
+  final String title;
+  final int amount;
+  final double unitPrice;
+
+  const PurchasedBook({
+    required this.title,
+    required this.amount,
+    required this.unitPrice,
+  });
+
+  PurchasedBook.fromEntry(Map entry) :
+      title = entry['title'],
+      amount = entry['amount'],
+      unitPrice = entry['unitPrice'];
+}
