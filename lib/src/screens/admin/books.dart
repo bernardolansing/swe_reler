86cca003795eda6d9ab21344b95df84c852d6009
Bernardo Lansing <bernardolans@gmail.com>
2024-02-04 12:20:18 -0300

    modularization of table widget, removed admin assets

--- lib/src/screens/admin/books.dart
+++ lib/src/screens/admin/books.dart
@@ -1,372 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:swe_reler/src/screens/admin/table.dart';
 
 class AdminBooks extends StatelessWidget {
   const AdminBooks({super.key});
 
   @override
   Widget build(BuildContext context) => Column(
-    crossAxisAlignment: CrossAxisAlignment.end,
+    crossAxisAlignment: CrossAxisAlignment.stretch,
     children: [
-      ElevatedButton(
-          onPressed: () {},
-          child: const Text('Cadastrar livro')
+      // We put the button inside a Row in order to keep it from stretching
+      // along with the table.
+      Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          ElevatedButton(
+              onPressed: () {},
+              child: const Text('Cadastrar livro')
+          ),
+        ],
       ),
 
       const SizedBox(height: 24),
 
-      Table(
-        border: const TableBorder(
-          top: _tableBorder,
-          bottom: _tableBorder,
-          left: _tableBorder,
-          right: _tableBorder,
-          horizontalInside: _tableBorder,
-        ),
-        children: [
-          TableRow(children: [
-            Container(
-              color: const Color.fromARGB(156, 77, 119, 84),
-              child: const Text("capa",
-                  style: TextStyle(color: Colors.white)),
-            ),
-            Container(
-              color: const Color.fromARGB(156, 77, 119, 84),
-              child: const Text("título",
-                  style: TextStyle(color: Colors.white)),
-            ),
-            Container(
-              color: const Color.fromARGB(156, 77, 119, 84),
-              child: const Text("autor",
-                  style: TextStyle(color: Colors.white)),
-            ),
-            Container(
-              color: const Color.fromARGB(156, 77, 119, 84),
-              child: const Text("editora",
-                  style: TextStyle(color: Colors.white)),
-            ),
-            Container(
-              color: const Color.fromARGB(156, 77, 119, 84),
-              child: const Text("ano",
-                  style: TextStyle(color: Colors.white)),
-            ),
-            Container(
-              color: const Color.fromARGB(156, 77, 119, 84),
-              child: const Text("preço",
-                  style: TextStyle(color: Colors.white)),
-            ),
-            Container(
-              color: const Color.fromARGB(156, 77, 119, 84),
-              child: const Text("avaliação",
-                  style: TextStyle(color: Colors.white)),
-            ),
-            Container(
-              color: const Color.fromARGB(156, 77, 119, 84),
-              child: const Text("qtd",
-                  style: TextStyle(color: Colors.white)),
-            ),
-            Container(
-              color: const Color.fromARGB(156, 77, 119, 84),
-              child: const Text("   ",
-                  style: TextStyle(color: Colors.white)),
-            ),
-          ]),
-          TableRow(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: AspectRatio(
-                    aspectRatio: 1,
-                    child: Image.asset(
-                      'assets/admin/lady-killers.png',
-                      fit: BoxFit.contain,
-                    ),
-                  ),
-                ),
-              ),
-              const Text(
-                  "Lady Killers: Assassinas em Série"),
-              const Text("Tori Telfer"),
-              const Text("Darkside"),
-              const Text("2019"),
-              const Text("45,99"),
-              const Text("4,9"),
-              const Text("3"),
-              Column(
-                children: [
-                  InkWell(
-                    onTap: () {},
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SizedBox(
-                        width: 30,
-                        height: 30,
-                        child: FittedBox(
-                          fit: BoxFit.contain,
-                          child: Image.asset(
-                              'assets/admin/edit.png'),
-                        ),
-                      ),
-                    ),
-                  ),
-                  InkWell(
-                    onTap: () {},
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SizedBox(
-                        width: 30,
-                        height: 30,
-                        child: FittedBox(
-                          fit: BoxFit.contain,
-                          child: Image.asset(
-                              'assets/admin/delete.png'),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          TableRow(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: AspectRatio(
-                    aspectRatio: 1,
-                    child: Image.asset(
-                      'assets/admin/lady-killers.png',
-                      fit: BoxFit.contain,
-                    ),
-                  ),
-                ),
-              ),
-              const Text(
-                  "Lady Killers: Assassinas em Série"),
-              const Text("Tori Telfer"),
-              const Text("Darkside"),
-              const Text("2019"),
-              const Text("45,99"),
-              const Text("4,9"),
-              const Text("3"),
-              Column(
-                children: [
-                  InkWell(
-                    onTap: () {},
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SizedBox(
-                        width: 30,
-                        height: 30,
-                        child: FittedBox(
-                          fit: BoxFit.contain,
-                          child: Image.asset(
-                              'assets/admin/edit.png'),
-                        ),
-                      ),
-                    ),
-                  ),
-                  InkWell(
-                    onTap: () {},
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SizedBox(
-                        width: 30,
-                        height: 30,
-                        child: FittedBox(
-                          fit: BoxFit.contain,
-                          child: Image.asset(
-                              'assets/admin/delete.png'),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          TableRow(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: AspectRatio(
-                    aspectRatio: 1,
-                    child: Image.asset(
-                      'assets/admin/lady-killers.png',
-                      fit: BoxFit.contain,
-                    ),
-                  ),
-                ),
-              ),
-              const Text(
-                  "Lady Killers: Assassinas em Série"),
-              const Text("Tori Telfer"),
-              const Text("Darkside"),
-              const Text("2019"),
-              const Text("45,99"),
-              const Text("4,9"),
-              const Text("3"),
-              Column(
-                children: [
-                  InkWell(
-                    onTap: () {},
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SizedBox(
-                        width: 30,
-                        height: 30,
-                        child: FittedBox(
-                          fit: BoxFit.contain,
-                          child: Image.asset(
-                              'assets/admin/edit.png'),
-                        ),
-                      ),
-                    ),
-                  ),
-                  InkWell(
-                    onTap: () {},
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SizedBox(
-                        width: 30,
-                        height: 30,
-                        child: FittedBox(
-                          fit: BoxFit.contain,
-                          child: Image.asset(
-                              'assets/admin/delete.png'),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          TableRow(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: AspectRatio(
-                    aspectRatio: 1,
-                    child: Image.asset(
-                      'assets/admin/lady-killers.png',
-                      fit: BoxFit.contain,
-                    ),
-                  ),
-                ),
-              ),
-              const Text(
-                  "Lady Killers: Assassinas em Série"),
-              const Text("Tori Telfer"),
-              const Text("Darkside"),
-              const Text("2019"),
-              const Text("45,99"),
-              const Text("4,9"),
-              const Text("3"),
-              Column(
-                children: [
-                  InkWell(
-                    onTap: () {},
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SizedBox(
-                        width: 30,
-                        height: 30,
-                        child: FittedBox(
-                          fit: BoxFit.contain,
-                          child: Image.asset(
-                              'assets/admin/edit.png'),
-                        ),
-                      ),
-                    ),
-                  ),
-                  InkWell(
-                    onTap: () {},
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SizedBox(
-                        width: 30,
-                        height: 30,
-                        child: FittedBox(
-                          fit: BoxFit.contain,
-                          child: Image.asset(
-                              'assets/admin/delete.png'),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          TableRow(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: AspectRatio(
-                    aspectRatio: 1,
-                    child: Image.asset(
-                      'assets/admin/lady-killers.png',
-                      fit: BoxFit.contain,
-                    ),
-                  ),
-                ),
-              ),
-              const Text(
-                  "Lady Killers: Assassinas em Série"),
-              const Text("Tori Telfer"),
-              const Text("Darkside"),
-              const Text("2019"),
-              const Text("45,99"),
-              const Text("4,9"),
-              const Text("3"),
-              Column(
-                children: [
-                  InkWell(
-                    onTap: () {},
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SizedBox(
-                        width: 30,
-                        height: 30,
-                        child: FittedBox(
-                          fit: BoxFit.contain,
-                          child: Image.asset(
-                              'assets/admin/edit.png'),
-                        ),
-                      ),
-                    ),
-                  ),
-                  InkWell(
-                    onTap: () {},
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SizedBox(
-                        width: 30,
-                        height: 30,
-                        child: FittedBox(
-                          fit: BoxFit.contain,
-                          child: Image.asset(
-                              'assets/admin/delete.png'),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ],
-      )
+      AdminTable(
+        headers: const ['título', 'autor', 'editora', 'ano', 'preço',
+          'avaliação', 'qtd'],
+        rows: List.generate(5, (index) => [
+          'Lady Killers: Assassinas em Série',
+          'Tori Telfer',
+          'Darkside',
+          '2019',
+          '45,99',
+          '4,9',
+          '3'
+        ]),
+        editAction: (index) {},
+        deleteAction: (index) {},
+      ),
     ],
-  );
-
-  static const _tableBorder = BorderSide(
-      color: Color.fromARGB(156, 77, 119, 84),
-      width: 1
   );
 }
